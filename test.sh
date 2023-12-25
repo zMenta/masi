127b3e84fa54695e48e1669d6ac065a8774f2ad4
moog <brentano.l@gmail.com>
2023-12-25 19:19:43 -0300

    making the test.sh the main file

--- test.sh
+++ test.sh
@@ -15,13 +15,42 @@ function printStatus { printf "\n\e[38;5;45m-- $1 --\e[0m\n"; }
 function printSucess { printf "\e[38;5;46m-- $1 --\e[0m\n"; }
 function printError { printf "\e[38;5;203m-- $1 --\e[0m\n"; }
 
+yayCheck() {
+	printStatus "Checking for yay"
+	ISYAY=/sbin/yay
+	if [ -f "$ISYAY" ]; then 
+		echo " -> yay was located, procceding"
+	else
+		printWarning "yay was NOT located"
+		echo " - Would you like to install yay?"
+
+		yn_options=("yes" "no")
+		singleselect yn_result yn_options
+		if [[ $yn_result == "yes" ]]; then
+			printStatus "installing yay"
+			git clone https://aur.archlinux.org/yay.git
+			cd yay
+			makepkg -si --noconfirm
+			cd ..
+			printSucess "done"
+		else
+			printError "yay is required for this script, exiting"
+			exit
+		fi
+	fi
+}
 
 # Script starts
+clear
+printStatus "Welcome to Masi"
+echo
+
 opts=("Install" "Update" "Exit")
 singleselect result opts
 
 if [ $result == 'Install' ]; then
 	echo "Installation began"
+	yayCheck
 elif [ $result == 'Update' ]; then
 	echo "Updating with the latest config files"
 else
